ef118dd445c172b172c632524a3922ea5c7b4e71
Mariainesrocha <57458721+Mariainesrocha@users.noreply.github.com>
2022-01-27 13:33:10 +0000

    Delete voice_recognition.py

--- voice_recognition.py
+++
@@ -1,9 +0,0 @@
-import torchaudio
-from speechbrain.pretrained import EncoderClassifier
-from speechbrain.pretrained import SpeakerRecognition
-
-verification = SpeakerRecognition.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb", savedir="pretrained_models/spkrec-ecapa-voxceleb")
-signal,fs = torchaudio.load('./samples/audio_samples/ethan1.flac')
-signal2,fs = torchaudio.load('./samples/audio_samples/a1.wav')
-score, prediction = verification.verify_batch(signal, signal2)
-print(score, prediction)
